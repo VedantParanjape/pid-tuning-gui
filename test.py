d9e17316494be58effa804e0fa5288cfd3cf6ea7
Vedant Paranjape <vedantparanjape160201@gmail.com>
2020-01-19 19:11:07 +0530

    added sin/cosine plots to the plot widgets, start and stops on pressing corresponding buttons

--- test.py
+++ test.py
@@ -3,7 +3,10 @@ from PyQt5.QtWidgets import QApplication
 import sys
 import layout
 import serial.tools.list_ports
-
+import pyqtgraph
+import time
+import math
+import pyqtgraph as pg
 def SerialPorts():
     PortList = serial.tools.list_ports.comports()
 
@@ -29,11 +32,36 @@ class PIDApp(QtWidgets.QMainWindow, layout.Ui_MainWindow):
         self.Kp = 0
         self.Ki = 0
         self.Kd = 0
+        self.timer = pg.QtCore.QTimer(self)
+        self.prev_i = 0
+
+    def update_plot(self):
+        X = [float(i/0.01) for i in range(self.prev_i, self.prev_i+2000)]
+        self.prev_i = self.prev_i + 2000
+        Y = [math.sin(X[i]) for i in range(2000)]
+        Y1 = [math.cos(X[i]) for i in range(2000)]
+        CenterPoint = self.prev_i - 1000
+        self.PlotWidgetUpLeft.setXRange(float(CenterPoint-5)/0.01, float(CenterPoint+5)/0.01)
+        self.PlotWidgetUpRight.setXRange(float(CenterPoint-5)/0.01, float(CenterPoint+5)/0.01)
+        self.PlotWidgetBottomLeft.setXRange(float(CenterPoint-5)/0.01, float(CenterPoint+5)/0.01)
+        self.PlotWidgetBottomCenter.setXRange(float(CenterPoint-5)/0.01, float(CenterPoint+5)/0.01)
+        self.PlotWidgetBottomRight.setXRange(float(CenterPoint-5)/0.01, float(CenterPoint+5)/0.01)
         
+        time.sleep(0.01)
+        self.PlotWidgetUpLeft.plot(X,Y, clear=True)
+        self.PlotWidgetUpRight.plot(X,Y1, clear=True)
+        self.PlotWidgetBottomLeft.plot(X,Y, clear=True)
+        self.PlotWidgetBottomCenter.plot(X,Y1, clear=True)
+        self.PlotWidgetBottomRight.plot(X,Y, clear=True)
+
+
     def StartButtonClick(self):
         print("start button clicked")
-    
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start(0)
+
     def StopButtonClick(self):
+        self.timer.stop()
         print("stop button clicked")
 
     def RefreshPortList(self):
@@ -47,15 +75,15 @@ class PIDApp(QtWidgets.QMainWindow, layout.Ui_MainWindow):
         print("set point: {}".format(self.SetPoint))
 
     def KpSendButtonClick(self):
-        self.KpLabel.setText("Kp: " + str(self.Kp))
+        self.KpLabel.setText(self.KpLabel.text()[0:8] + str(self.Kp))
         print("kp: {}".format(self.Kp))
 
     def KiSendButtonClick(self):
-        self.KiLabel.setText("Ki: " + str(self.Ki))
+        self.KiLabel.setText(self.KiLabel.text()[0:8] + str(self.Ki))
         print("ki: {}".format(self.Ki))
     
     def KdSendButtonClick(self):
-        self.KdLabel.setText("Kd: " + str(self.Kd))
+        self.KdLabel.setText(self.KdLabel.text()[0:8] + str(self.Kd))
         print("kd: {}".format(self.Kd))
 
     def SetPointChanged(self):
