42c5487fa39f7904e46a92be997ba28b5aff2458
Shreyas <shreyasatre16@gmail.com>
2021-01-26 01:39:45 +0530

    Basic server functions

--- cpp_app/include/libserver/server.hpp
+++ cpp_app/include/libserver/server.hpp
@@ -4,25 +4,42 @@
 #include <boost/thread.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
+#include <boost/property_tree/json_parser.hpp>
 #include <iostream>
+
+#define _MAX_BUF_SIZE_ 512
 
 namespace libserver 
 {   
-    class server :public boost::enable_shared_from_this<server>
+    class server :public boost::enable_shared_from_this<libserver::server>
     {
         private:
-        /**/
             boost::asio::ip::tcp::socket tcp_socket;
             boost::asio::ip::udp::socket udp_socket;
-            void _read_from_socket();
-            std::string _incomming_data;
+            char _incomming_data[_MAX_BUF_SIZE_];
+            unsigned int _num_bytes_read; 
+            boost::property_tree::ptree json_tree;
         public:
         /**/
+            void handler();
+            void read();
+            // void send(std::string data);
+            // void shutdown();    
+            // boost::asio::ip::tcp::socket& get_sock();
             server();
-            void display();
-            boost::asio::ip::tcp::socket& _get_tcp_socket();
-            boost::asio::ip::udp::socket& _get_udp_socket();
-            void tcp_read();
+            // size_t port;
     };
-    void handler();
+
+    class pid_const_data:public boost::enable_shared_from_this<server>
+    {
+        public:
+            double ki;
+            double kp;
+            double kd;
+            double set_point;
+            pid_const_data();
+            void parse();
+    };
+    void acceptor();
+    void main_handler();
 }
